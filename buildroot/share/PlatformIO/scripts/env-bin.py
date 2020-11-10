320607fd806269c4cc683ba6d66a0be31983f6f3
Alexander D. Kanevskiy <kad@kad.name>
2020-11-10 22:55:00 +0200

    add version to filename

--- buildroot/share/PlatformIO/scripts/env-bin.py
+++ buildroot/share/PlatformIO/scripts/env-bin.py
@@ -1,3 +1,6 @@
 Import("env")
 
-env.Replace(PROGNAME="%s-firmware" % env['PIOENV'])
+if 'MARLIN_FEATURES' in env and 'SHORT_BUILD_VERSION' in env['MARLIN_FEATURES']:
+    env.Replace(PROGNAME="%s-%s-firmware" % (env['PIOENV'], env['MARLIN_FEATURES']['SHORT_BUILD_VERSION'].strip('"')))
+else:
+    env.Replace(PROGNAME="%s-firmware" % (env['PIOENV'],))
