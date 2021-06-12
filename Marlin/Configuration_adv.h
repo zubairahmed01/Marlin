ae4dfd385c20310848e09d420ac1aa23851a10e9
Alexander D. Kanevskiy <kad@kad.name>
2021-06-12 23:08:46 +0300

    enable HOST_ACTION_COMMANDS, issue #19

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3878,7 +3878,9 @@
  * Host Prompt Support enables Marlin to use the host for user prompts so
  * filament runout and other processes can be managed from the host side.
  */
-//#define HOST_ACTION_COMMANDS
+#if ENABLED(KAD_SKR_ENOUGH_FLASH)
+  #define HOST_ACTION_COMMANDS
+#endif
 #if ENABLED(HOST_ACTION_COMMANDS)
   //#define HOST_PROMPT_SUPPORT
   //#define HOST_START_MENU_ITEM  // Add a menu item that tells the host to start
