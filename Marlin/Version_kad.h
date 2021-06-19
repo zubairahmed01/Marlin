c19590e24843fdde9f0c0558be9565da612be1a7
Alexander D. Kanevskiy <kad@kad.name>
2021-06-19 20:50:33 +0300

    Bump version

--- Marlin/Version_kad.h
+++ Marlin/Version_kad.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.8.2-kad1"
+#define SHORT_BUILD_VERSION "2.0.9-kad1"
 
 /**
  * Verbose version identifier which should contain a reference to the location
