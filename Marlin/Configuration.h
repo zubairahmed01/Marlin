9db56152cb56fbb241d60e2c4540819108d9a394
Zubair Ahmed <zubair1291@gmail.com>
2024-12-15 22:17:47 +0100

    fix bed size and poisiton from KAD config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1878,12 +1878,12 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 225
+#define Y_BED_SIZE 225
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -9
-#define Y_MIN_POS 0
+#define X_MIN_POS -7
+#define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
